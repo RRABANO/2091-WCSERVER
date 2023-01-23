7dc92cf471a3758316ee60a97e8caec74e4d92c6
Rabano-Rymbrent <123344347+RRABANO@users.noreply.github.com>
2023-01-23 11:58:25 +0800

    Add files via upload

---
+++ Prelims/node-example.js
@@ -0,0 +1,12 @@
+//Rabano, Rymbrent Maximillian L.
+//Module 3
+//Week 2 and 3 Basics of Node.JS
+
+//First Program
+console.log("Welcome to WCSERVER!!!"); 
+
+//creating my first function
+function Display(x){
+    console.log(x);
+}
+Display(200);
