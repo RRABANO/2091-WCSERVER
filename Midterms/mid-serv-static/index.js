0bbefa507baf5e1f6b7a2e092316ab76e7a6b21a
RRABANO <123344347+RRABANO@users.noreply.github.com>
2023-02-20 12:18:28 +0800

    1 binaries uploaded

--- Midterms/mid-serv-static/index.js
+++ Midterms/mid-serv-static/index.js
@@ -11,4 +11,22 @@ app.get('/',(req,res)=>{
 const port = process.env.PORT || 3000;
 app.listen(port, ()  => {
     console.log(`'Listening on part ${port}'`);
-});
+});3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
